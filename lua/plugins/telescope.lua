5c7e909812f42c1565aa9a598981b83d25ea8b65
Zach <zijiebing@gmail.com>
2024-10-25 22:42:11 +0800

    [Config] update telescope sorter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -55,5 +55,13 @@ return {
         },
       },
     },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
+      }
+    }
   },
 }
