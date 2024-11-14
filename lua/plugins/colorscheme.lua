b75988574b4c90b7dbccc4163215b1ca5c6afa0c
Zach <zijiebing@gmail.com>
2024-11-14 20:12:03 +0800

    [Update](Plugin) Make tokyonight transparent.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,17 @@ return {
   { "ellisonleao/gruvbox.nvim" },
 
   {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight",
