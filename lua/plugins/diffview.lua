841c39bce2db90b3d9becc73f1d9fda569c524f9
Zach <zijiebing@gmail.com>
2025-02-19 14:31:18 +0800

    (update)plugins: remove diffview & upgrade plugins.

--- lua/plugins/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "sindrets/diffview.nvim",
-    cmd = "DiffviewFileHistory %",
-    keys = {
-      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview" },
-    },
-  },
-}
