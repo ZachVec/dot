cbfae07c8fa40dd2d22abcd5dd7bb0594970ecde
Zach <zijiebing@gmail.com>
2025-03-19 12:59:51 +0800

    (remove)plugins: replace tabpicker with fzf-lua

--- lua/plugins/tabpicker.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ZachVec/tabpicker.nvim",
-  events = "VeryLazy",
-  keys = {
-    { "<leader>;<tab>", "<cmd>lua require('tabpicker').find_tabpages()<cr>", desc = "Tabs" },
-  },
-  opts = {},
-}
