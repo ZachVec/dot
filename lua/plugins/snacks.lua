bbebd16cc0b0a5bc3ee16dcc0c56fa2955de4b8b
Zach <zijiebing@gmail.com>
2025-07-23 00:42:03 +0800

    feat(snacks.dashboard): replace recent files with recent projects on dashboard.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,26 @@
+local dashboard = {
+  preset = {
+    --- stylua: ignore
+    keys = {
+      { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+      { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+      { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+      { icon = " ", key = "r", desc = "Recent Projects", action = ":lua require('persistence').select()" },
+      { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+      { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+      { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+    },
+    header = [[
+ ██╗   ██╗███████╗     ██████╗ ██████╗ ██████╗ ███████╗
+ ██║   ██║██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝
+ ██║   ██║███████╗    ██║     ██║   ██║██║  ██║█████╗  
+ ╚██╗ ██╔╝╚════██║    ██║     ██║   ██║██║  ██║██╔══╝  
+  ╚████╔╝ ███████║    ╚██████╗╚██████╔╝██████╔╝███████╗
+   ╚═══╝  ╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝]],
+  },
+}
+
 return {
   {
     "folke/snacks.nvim",
@@ -13,25 +36,15 @@ return {
           border = "rounded",
         },
       },
-      dashboard = {
-        preset = {
-          header = [[
- ██╗   ██╗███████╗     ██████╗ ██████╗ ██████╗ ███████╗
- ██║   ██║██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝
- ██║   ██║███████╗    ██║     ██║   ██║██║  ██║█████╗  
- ╚██╗ ██╔╝╚════██║    ██║     ██║   ██║██║  ██║██╔══╝  
-  ╚████╔╝ ███████║    ╚██████╗╚██████╔╝██████╔╝███████╗
-   ╚═══╝  ╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝]]
-        }
-      },
+      dashboard = dashboard,
       indent = {
         indent = {
-          only_scope = true
+          only_scope = true,
         },
         chunk = {
-          enabled = true
+          enabled = true,
         },
-      }
+      },
     },
   },
 }
