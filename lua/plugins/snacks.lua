c83c594087ead0b8264a070bd3b1f7f58b7784b2
Zach <zijiebing@gmail.com>
2025-07-27 23:06:02 +0800

    feat(snacks.dashboard): map Lazy to l instead of L

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,7 +8,7 @@ local dashboard = {
       { icon = " ", key = "r", desc = "Recent Projects", action = ":lua require('persistence').select()" },
       { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
       { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-      { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+      { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
       { icon = " ", key = "q", desc = "Quit", action = ":qa" },
     },
     header = [[
