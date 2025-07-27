aee9324b5641a42068b78dd3a111dbcff995d9aa
Zach <zijiebing@gmail.com>
2025-07-27 23:06:53 +0800

    feat(neo-tree): unmap & remap some keymaps

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -36,6 +36,97 @@ return {
   opts = function(_, opts)
     table.insert(opts.sources, "document_symbols")
     opts = vim.tbl_deep_extend("force", opts, {
+      window = {
+        mappings = {
+          ["."] = "noop",
+          ["b"] = "noop",
+          ["f"] = "noop",
+          ["h"] = "noop",
+          ["i"] = "noop",
+          ["l"] = "noop",
+          ["o"] = "noop",
+          ["z"] = "noop",
+          ["O"] = "noop",
+          ["C"] = "noop",
+          ["oc"] = "noop",
+          ["od"] = "noop",
+          ["og"] = "noop",
+          ["om"] = "noop",
+          ["on"] = "noop",
+          ["os"] = "noop",
+          ["ot"] = "noop",
+          ["<bs>"] = "noop",
+          ["e"] = "toggle_node",
+          ["="] = "toggle_auto_expand_width",
+          ["zC"] = "close_all_nodes",
+          ["zc"] = "close_node",
+          ["zR"] = "expand_all_nodes",
+          ["zo"] = "expand_all_subnodes",
+        },
+      },
+      filesystem = {
+        window = {
+          mappings = {
+            ["#"] = "noop",
+            ["A"] = "noop",
+            ["D"] = "noop",
+            ["]g"] = "noop",
+            ["[g"] = "noop",
+            ["<C-x>"] = "noop",
+          },
+        },
+      },
+      buffers = {
+        window = {
+          mappings = {
+            ["bd"] = "noop",
+          },
+        },
+      },
+      git_status = {
+        window = {
+          mappings = {
+            ["c"] = "noop",
+            ["d"] = "noop",
+            ["m"] = "noop",
+            ["p"] = "noop",
+            ["s"] = "noop",
+            ["t"] = "noop",
+            ["w"] = "noop",
+            ["x"] = "noop",
+            ["y"] = "noop",
+            ["S"] = "noop",
+            ["ga"] = "noop", -- stage
+            ["gc"] = "noop", -- commit
+            ["gg"] = "noop", -- commit and push
+            ["gp"] = "noop", -- push
+            ["gr"] = "noop", -- revert
+            ["gu"] = "noop", -- unstage
+            ["gU"] = "noop", -- Undo last commit
+            ["a"] = {
+              function(state)
+                local gcmd = require("neo-tree.sources.git_status.commands")
+                gcmd.git_add_all(state)
+              end,
+              desc = "git add file",
+            },
+            ["r"] = {
+              function(state)
+                local gcmd = require("neo-tree.sources.git_status.commands")
+                gcmd.git_revert_file(state)
+              end,
+              desc = "git restore file",
+            },
+            ["u"] = {
+              function(state)
+                local gcmd = require("neo-tree.sources.git_status.commands")
+                gcmd.git_unstage_file(state)
+              end,
+              desc = "git unstage file",
+            },
+          },
+        },
+      },
       default_component_configs = {
         indent = {
           indent_size = 1,
