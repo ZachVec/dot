e46f912deb24ef022db0b40acfec5448d3fdf536
Zach <zijiebing@gmail.com>
2025-08-16 18:45:35 +0800

    feat(fruit & mark): introduce fruits.nvim and neo-tree-marks.nvim

---
+++ lua/plugins/fruits.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "ZachVec/fruits.nvim",
+    event = "BufWinEnter",
+    -- stylua: ignore
+    keys = {
+      -- available colors: charcoal yellow menthe purple red green blue pink latte brown
+      { "<leader>h1", function () require("fruits").highlight:set("Pink") end, mode = { "n", "v" }, desc = "highlight" },
+      { "<leader>hd", function () require("fruits").highlight:reset() end, mode = { "n", "v" }, desc = "remove" },
+    },
+    opts = {
+      highlight = {
+        enable = true,
+      },
+      mark = {
+        enable = true,
+      },
+    },
+    dev = false,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.mark = {}
+      table.insert(opts.sources, "mark")
+      return opts
+    end,
+  },
+  {
+    "ZachVec/neo-tree-mark.nvim",
+    dependencies = {
+      { "nvim-neo-tree/neo-tree.nvim" },
+      { "ZachVec/fruits.nvim" },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>ma", function () require("neo-tree.sources.mark"):create_mark() end, desc = "Create Mark at cursor" },
+      { "<leader>em", function () require("neo-tree.command").execute({ source = "mark", toggle = true }) end, desc = "Mark Explorer" }
+    },
+    dev = false,
+  },
+}
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,38 +1,18 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  keys = function()
-    -- stylua: ignore
-    return {
-      {
-        "<leader>ef",
-        function()
-          require("neo-tree.command").execute({ source = "filesystem", toggle = true, dir = LazyVim.root() })
-        end,
-        desc = "Explorer NeoTree (Root Dir)",
-      },
-      {
-        "<leader>eg",
-        function()
-          require("neo-tree.command").execute({ source = "git_status", toggle = true })
-        end,
-        desc = "Git Explorer",
-      },
-      {
-        "<leader>eb",
-        function()
-          require("neo-tree.command").execute({ source = "buffers", toggle = true })
-        end,
-        desc = "Buffer Explorer",
-      },
-      {
-        "<leader>es",
-        function()
-          require("neo-tree.command").execute({ source = "document_symbols", toggle = true })
-        end,
-        desc = "Symbol Explorer",
-      },
-    }
-  end,
+  -- stylua: ignore
+  keys = {
+    { "<leader>ef", function() require("neo-tree.command").execute({ source = "filesystem", toggle = true, dir = LazyVim.root() }) end, desc = "Explorer NeoTree (Root Dir)" },
+    { "<leader>eg", function() require("neo-tree.command").execute({ source = "git_status", toggle = true }) end, desc = "Git Explorer" },
+    { "<leader>eb", function() require("neo-tree.command").execute({ source = "buffers", toggle = true }) end, desc = "Buffer Explorer" },
+    { "<leader>es", function() require("neo-tree.command").execute({ source = "document_symbols", toggle = true }) end, desc = "Symbol Explorer" },
+    { "<leader>e", false },
+    { "<leader>E", false },
+    { "<leader>be", false },
+    { "<leader>fe", false },
+    { "<leader>fE", false },
+    { "<leader>gE", false },
+  },
   opts = function(_, opts)
     table.insert(opts.sources, "document_symbols")
     opts = vim.tbl_deep_extend("force", opts, {
