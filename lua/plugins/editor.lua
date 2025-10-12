7e9acf622f9f9a3f8d54c14ddc41046cc6dd0e7c
Zach <zijiebing@gmail.com>
2025-10-12 23:49:48 +0800

    feat(editor): preview via snacks explorer in a float window, and bind <esc> to close help and preview.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,13 +4,86 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
-        layout = {
-          preset = "ivy",
-        },
         sources = {
           --- @type snacks.picker.explorer.Config
           explorer = {
-            -- layout = { preset = "ivy", preview = true },
+            on_show = function(picker)
+              local show = false
+              local gap = 1
+              local clamp_width = function(value)
+                return math.max(20, math.min(100, value))
+              end
+              local position = picker.resolved_layout.layout.position
+              local rel = picker.layout.root
+              local update = function(win) ---@param win snacks.win
+                local border = win:border_size().left + win:border_size().right
+                win.opts.row = vim.api.nvim_win_get_position(rel.win)[1]
+                win.opts.height = 0.8
+                if position == "left" then
+                  win.opts.col = vim.api.nvim_win_get_width(rel.win) + gap
+                  win.opts.width =
+                    clamp_width(vim.o.columns - border - win.opts.col)
+                end
+                if position == "right" then
+                  win.opts.col = -vim.api.nvim_win_get_width(rel.win) - gap
+                  win.opts.width =
+                    clamp_width(vim.o.columns - border + win.opts.col)
+                end
+                win:update()
+              end
+              local preview_win = Snacks.win.new({
+                relative = "editor",
+                external = false,
+                focusable = false,
+                border = "rounded",
+                backdrop = false,
+                show = show,
+                bo = {
+                  filetype = "snacks_float_preview",
+                  buftype = "nofile",
+                  buflisted = false,
+                  swapfile = false,
+                  undofile = false,
+                },
+                on_win = function(win)
+                  update(win)
+                  picker:show_preview()
+                end,
+              })
+              rel:on("WinLeave", function()
+                vim.schedule(function()
+                  if not picker:is_focused() then
+                    picker.preview.win:close()
+                  end
+                end)
+              end)
+              rel:on("WinResized", function()
+                update(preview_win)
+              end)
+              picker.preview.win = preview_win
+              picker.main = preview_win.win
+            end,
+            on_close = function(picker)
+              picker.preview.win:close()
+            end,
+            layout = { preset = "sidebar", preview = false },
+            actions = {
+              toggle_preview = function(picker)
+                picker.preview.win:toggle()
+              end,
+              close_preview = function(picker)
+                picker.preview.win:close()
+              end,
+              close_help = function()
+                for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+                  local buf = vim.api.nvim_win_get_buf(win)
+                  if vim.bo[buf].filetype == "snacks_win_help" then
+                    vim.api.nvim_win_close(win, true)
+                    return
+                  end
+                end
+              end,
+            },
             win = {
               input = {
                 keys = {
@@ -72,7 +145,7 @@ return {
                   -- ["<2-LeftMouse>"] = "confirm",
                   -- ["<CR>"] = "confirm",
                   ["<Down>"] = false,
-                  ["<Esc>"] = false,
+                  ["<Esc>"] = { { "close_preview", "close_help" } },
                   ["<S-CR>"] = false,
                   ["<S-Tab>"] = false,
                   -- ["<Tab>"] = { "select_and_next", mode = { "n", "x" } },
