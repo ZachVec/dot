bc4e136a7aa8c2497392df1721b7160946365a52
Zach <zijiebing@gmail.com>
2025-10-12 17:30:16 +0800

    style(ui): format codes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,25 +1,3 @@
-local dashboard = {
-  preset = {
-    -- stylua: ignore
-    keys = {
-      { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-      { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-      { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-      { icon = " ", key = "r", desc = "Recent Projects", action = ":lua require('persistence').select()" },
-      { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-      { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-      { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-    },
-    header = [[
- ██╗   ██╗███████╗     ██████╗ ██████╗ ██████╗ ███████╗
- ██║   ██║██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝
- ██║   ██║███████╗    ██║     ██║   ██║██║  ██║█████╗  
- ╚██╗ ██╔╝╚════██║    ██║     ██║   ██║██║  ██║██╔══╝  
-  ╚████╔╝ ███████║    ╚██████╗╚██████╔╝██████╔╝███████╗
-   ╚═══╝  ╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝]],
-  },
-}
 return {
   {
     "nvim-lualine/lualine.nvim",
@@ -103,7 +81,28 @@ return {
           border = "rounded",
         },
       },
-      dashboard = dashboard,
+      dashboard = {
+        preset = {
+          -- stylua: ignore
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Projects", action = ":lua require('persistence').select()" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+          header = [[
+ ██╗   ██╗███████╗     ██████╗ ██████╗ ██████╗ ███████╗
+ ██║   ██║██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝
+ ██║   ██║███████╗    ██║     ██║   ██║██║  ██║█████╗  
+ ╚██╗ ██╔╝╚════██║    ██║     ██║   ██║██║  ██║██╔══╝  
+  ╚████╔╝ ███████║    ╚██████╗╚██████╔╝██████╔╝███████╗
+   ╚═══╝  ╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝]],
+        },
+      },
       indent = {
         indent = {
           only_scope = true,
